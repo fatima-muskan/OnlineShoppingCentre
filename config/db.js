e85e7ee0fdfaf4165e3b8e00cb9095c0ab394aa2
MukarramDaniel2023 <dfedors000@gmail.com>
2023-11-08 01:37:09 +0500

    UserSchema

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import colors from 'colors';
 const connectDB = async () => {
     try{    
         const conn=await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to MongoDb databse ${conn.connection.host}`);
+        console.log(`Connected to MongoDb databse ${conn.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`Error in MongoDb ${error}`.bgRed.white)
     }
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,33 @@
 import mongoose, { Mongoose } from 'mongoose';
 
 const userSchema=new Mongoose.Schema({
-    
-})
+    name:{
+        type: String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    role:{
+        required:true,
+        default:0
+    }
+},{timestamps:true}
+);
+
+export default mongoose.model('users',userSchema)
