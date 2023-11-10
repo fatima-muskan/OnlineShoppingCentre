96d7693e48f0997f4579d4d239dcd6c2a792b336
MukarramDaniel2023 <dfedors000@gmail.com>
2023-11-11 01:07:22 +0500

    Register API

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan'
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
 
 // configure env
 dotenv.config();
@@ -16,6 +17,8 @@ const app=express()
 // middleware
 app.use(express.json())
 app.use(morgan('dev'));
+
+app.get('/api/v1/auth', authRoutes)
 
 //rest api
 app.get('/', (req,res) => {
