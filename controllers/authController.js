36817d45e26d98677e961671a43f648b5c991c73
MukarramDaniel2023 <dfedors000@gmail.com>
2023-11-12 13:38:35 +0500

    Registration Successful

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,14 @@
-export const registerController = () => {};
+import userModel from "../models/userModel";
+
+export const registerController = async (req,res) => {
+    try {
+        const {name,email,password,phone,address}=req.body;
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:'Error in Registration',
+            error
+        })
+    }
+};
