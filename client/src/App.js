8245fcccfc04ee1d704024073403d6d6c588d5c2
MukarramDaniel2023 <dfedors000@gmail.com>
2023-11-24 21:53:20 +0500

    Admin Panel

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import PrivateRoute from './components/Routes/Private';
 import ForgotPassword from './pages/Auth/ForgotPassword';
 import AdminRoute from './components/Routes/AdminRoute';
 import AdminDashboard from './pages/Admin/AdminDashboard';
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
 
 
 function App() {
@@ -23,6 +25,9 @@ function App() {
         </Route>
         <Route path='/dashboard' element={<AdminRoute/>}>
           <Route path='admin' element={<AdminDashboard/>}/>
+          <Route path='admin/create-category' element={<CreateCategory/>}/>
+          <Route path='admin/create-product' element={<CreateProduct/>}/>
+          
         </Route>
         <Route path="/register" element={<Register />}/>
         <Route path="/forgot-password" element={<ForgotPassword />}/>
--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -2,7 +2,7 @@
 import React,{useState,useEffect} from 'react';
 import { useNavigate,useLocation } from 'react-router-dom';
 
-const Spinner = () => {
+const Spinner = ({path="login"}) => {
     const [count,setCount]=useState(3);
     const navigate=useNavigate();
     const location=useLocation();
@@ -11,11 +11,11 @@ const Spinner = () => {
         const interval=setInterval(() => {
             setCount((prevValue) => --prevValue)
         },1000);
-        count ===0 && navigate('/login',{
+        count ===0 && navigate(`/${path}`,{ // Redirect to the path if it loads a page that is not allowed to view to user
             state:location.pathname
         });
         return ()=> clearInterval(interval);
-    },[count,navigate,location])
+    },[count,navigate,location,path])
   return (
     <>
     <div class="d-flex flex-column justify-content-center align-items-center" style={{height:"70vh"}}>
