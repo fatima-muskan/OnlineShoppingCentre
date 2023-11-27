610091a1115cd49510e9b5a0c0ef1f6ee7f657d0
MukarramDaniel2023 <dfedors000@gmail.com>
2023-11-28 01:19:52 +0500

    Modify HomePage
    In this commit:
    
    I showed the products to the user. User can apply filters of categories and prices. Products will be shown according to the filters.

---
+++ client/src/components/Prices.js
@@ -0,0 +1,27 @@
+export const Prices=[
+    {
+        _id:0,
+        name:"PKR 0 to 4,999",
+        array:[0,4999],
+    },
+    {
+        _id:1,
+        name:"PKR 5,000 to 9,999",
+        array:[5000,9999],
+    },
+    {
+        _id:2,
+        name:"PKR 10000 to 14,999",
+        array:[10000,14999],
+    },
+    {
+        _id:3,
+        name:"PKR 15000 to 19,999",
+        array:[15000,19999],
+    },
+    {
+        _id:4,
+        name:"PKR 20000 or more",
+        array:[20000,999999999],
+    },
+]
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,30 @@
 import React,{useState,useEffect} from 'react'
 import Layout from './../components/Layout/Layout';
-import { useAuth } from '../context/auth';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
-
+import {Checkbox,Radio} from 'antd';
+import { Prices } from '../components/Prices';
 const HomePage = () => {
-  const [auth,setAuth]=useAuth()
+  
   const [products,setProducts]=useState([])
   const [categories,setCategories]=useState([])
+  const [checked, setChecked]=useState([])
+  const [radio, setRadio]=useState([])
+
+  // Get all categories
+  const getAllCategory = async() => {
+    try {
+      const {data}=await axios.get('/api/v1/category/get-category');
+      if(data?.success){
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    getAllCategory();
+  },[]);
 
   // Get all Producst
   const getAllProducts=async()=>{
@@ -26,11 +42,45 @@ const HomePage = () => {
   useEffect(()=>{
     getAllProducts()
   },[])
+
+  // Filter by Categories
+  const handleFilter=(value,id)=>{
+    let all=[...checked]
+    if(value){
+      all.push(id)
+    }
+    else{
+      all=all.filter(c=>c!== id);
+    }
+    setChecked(all)
+  }
   return (
     <Layout title={'Best Offers'}>
-        <div className='row'>
-          <div className='col-md-3'>
+        <div className='container-fluid row mt-3'>
+          <div className='col-md-2'>
             <h4 className='text-center'>Filter by Categories</h4>
+            <div className='d-flex flex-column'>
+
+            {
+              categories?.map((c) =>(
+                <Checkbox key={c._id} onChange={(e) => handleFilter(e.target.checked,c._id)}>
+                  {c.name}
+                </Checkbox>
+              ))
+            }
+            </div>
+            
+            <h4 className='text-center mt-4'>Filter by Price</h4>
+            <div className='d-flex flex-column'>
+              <Radio.Group onChange={e=>setRadio(e.target.value)}>
+                {Prices?.map(p=>(
+                  <div key={p._id}>
+
+                  <Radio value={p.array}>{p.name}</Radio>
+                  </div>
+                ))}
+              </Radio.Group>
+            </div>
           </div>
           <div className='col-md-9'>
             <h1 className='text-center'> All Products</h1>
@@ -42,7 +92,8 @@ const HomePage = () => {
                       <div className="card-body">
                         <h5 className="card-title">{p.name}</h5>
                         <p className="card-text">{p.description}</p>
-                        <button className='btn btn-primary'>More Details</button>
+                        <button className='btn btn-primary ms-1'>More Details</button>
+                        <button className='btn btn-secondary ms-1'>Add to Cart</button>
                       </div>
                     </div>
                     
